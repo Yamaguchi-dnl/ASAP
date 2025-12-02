b7ad1b182b092b9bf3767601a555b5d55c91aa08
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-02 17:04:50 +0000

    na hero tem um texto se sobrpondo ao outro ajuste isso

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -19,12 +19,12 @@ export function HeroSection() {
       )}
       <Container className="relative z-10 grid md:grid-cols-2 gap-8 items-center">
         <div className="text-left">
-          <p className="text-lg md:text-xl text-white mb-4">
-            Atuamos com soluções integradas que fortalecem a cultura corporativa e impulsionam transformações profissionais que sustentam ambientes de trabalho mais saudáveis no curto, médio e longo prazos.
-          </p>
           <h1 className="text-8xl sm:text-9xl md:text-[160px] font-black tracking-tighter text-white uppercase leading-none">
             PULSOASAP
           </h1>
+          <p className="text-lg md:text-xl text-white mb-4">
+            Atuamos com soluções integradas que fortalecem a cultura corporativa e impulsionam transformações profissionais que sustentam ambientes de trabalho mais saudáveis no curto, médio e longo prazos.
+          </p>
         </div>
         <div className="text-left text-white max-w-md md:ml-auto">
           <p>
