ee48f2e743bb1b97903971b1ae4f1fe45da9d44e
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-02 17:05:21 +0000

    ainda estão se sobrepondo

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -19,10 +19,10 @@ export function HeroSection() {
       )}
       <Container className="relative z-10 grid md:grid-cols-2 gap-8 items-center">
         <div className="text-left">
-          <h1 className="text-8xl sm:text-9xl md:text-[160px] font-black tracking-tighter text-white uppercase leading-none">
+           <h1 className="text-8xl sm:text-9xl md:text-[160px] font-black tracking-tighter text-white uppercase leading-none">
             PULSOASAP
           </h1>
-          <p className="text-lg md:text-xl text-white mb-4">
+          <p className="text-lg md:text-xl text-white">
             Atuamos com soluções integradas que fortalecem a cultura corporativa e impulsionam transformações profissionais que sustentam ambientes de trabalho mais saudáveis no curto, médio e longo prazos.
           </p>
         </div>
