7610c84daed4022208415d4ce9996920c37c93c9
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-08 16:20:24 +0000

    o titulo da seção pode ser QUEM SOMOS

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -86,7 +86,7 @@ export function AboutSection() {
           <div className="flex flex-col justify-center h-full">
              <MotionWrapper>
                <h2 className="text-5xl md:text-7xl font-bold text-foreground uppercase tracking-tighter">
-                Nossa Jornada
+                QUEM SOMOS
               </h2>
               <hr className="border-t-2 border-primary w-24 mt-4 mb-8" />
             </MotionWrapper>
