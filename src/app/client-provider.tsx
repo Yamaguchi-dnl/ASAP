7ba4dc6c5295989b2104f0d37a750de16d72489b
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-10 19:03:40 +0000

    Altere almude o texto do seja um patrocinadorgo

--- src/app/client-provider.tsx
+++ src/app/client-provider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
