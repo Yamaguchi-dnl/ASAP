0197434b440be09454528e8b109027c8f0256185
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-11 12:17:54 +0000

    ainda ta longe quero que fique mais perto

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -75,9 +75,9 @@ export function ServicesSection() {
             onMouseEnter={plugin.current.stop}
             onMouseLeave={plugin.current.reset}
           >
-             <div className="flex justify-end mb-4 -top-2 relative">
-                <CarouselPrevious className="relative top-0 h-12 w-12 rounded-full border-white text-white bg-transparent hover:bg-white/10" />
-                <CarouselNext className="relative top-0 right-0 h-12 w-12 rounded-full border-white text-white bg-transparent hover:bg-white/10" />
+             <div className="flex justify-end mb-4 relative -top-2 gap-2">
+                <CarouselPrevious className="static h-12 w-12 rounded-full border-white text-white bg-transparent hover:bg-white/10" />
+                <CarouselNext className="static h-12 w-12 rounded-full border-white text-white bg-transparent hover:bg-white/10" />
             </div>
 
             <CarouselContent className="-ml-4">
