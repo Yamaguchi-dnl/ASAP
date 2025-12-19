e10bdf882dcc1785f306b4d6148cb438ef87c43d
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-12-19 18:52:02 +0000

    favicon ainda n está funcionando pode ser pelo tramanho da imagem?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import { ClientProvider } from './client-provider';
 export const metadata: Metadata = {
   title: 'PulsoASAP',
   description: 'Promovendo saúde e bem-estar no ambiente de trabalho.',
-  icons: {
-    icon: '/favicon.ico?v=2',
-  },
 };
 
 export default function RootLayout({
@@ -17,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
         <link rel="preconnect" href="https://api.fontshare.com" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@400,700,800,900&display=swap"
