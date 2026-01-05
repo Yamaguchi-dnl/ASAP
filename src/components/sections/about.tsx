f8c22f402d3a3293be886ac8b5c5d19ad86cf7bd
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2026-01-05 20:21:01 +0000

    DEIXE ESSA FOTO 50 PX MENOR (_for element img_)

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -86,7 +86,7 @@ export function AboutSection() {
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Coluna da Imagem */}
-          <motion.div className="relative w-5/6 mx-auto aspect-[4/5] rounded-lg overflow-hidden" variants={imageVariants}>
+          <motion.div className="relative w-4/5 mx-auto aspect-[4/5] rounded-lg overflow-hidden" variants={imageVariants}>
             <AnimatePresence mode="wait">
               {founderImage && (
                 <motion.div
